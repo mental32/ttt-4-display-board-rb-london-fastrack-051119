3b6c8c858c949bb66d6b486c32b73208fc816579
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-11 10:48:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,21 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" "] * 9
+
+print board
+puts
+
+def display_board
+  board = [" "] * 9
+
+  for slice in board.each_slice(3).to_a do
+    puts " #{slice[0]} | #{slice[1]} | #{slice[2]} "
+    puts "-----------"
+  end
+
+end
+
+display_board
+
+puts
